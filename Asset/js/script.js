885d6f88e805789c9097b20b2fd35e3ad719f603
Vicheka Phat <vickphat@gmail.com>
2021-03-21 00:29:12 -0400

    added function to render first question in array

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -6,28 +6,77 @@ var choices = document.querySelector("#choices");
 var startButton = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
 var endScreen = document.querySelector("#end-screen");
-var nextQuestions;
+var choiceA = document.querySelector("#A")
+var choiceB = document.querySelector("#B")
+var choiceC = document.querySelector("#C")
+var choiceD = document.querySelector("#D")
 var currentIndex = 0;
 var score = 0;
 var secondsLeft = 75;
-var currentQuestionIndex = 0;
-var allScores = [];
-var storedScores = JSON.parse(localStorage.getItem("userData"));
+
+
+var quizQuestions = [
+    {
+        question : "What does JavaScript stand for?",
+        choiceA : "Java",
+        choiceB : "JavaScript",
+        choiceC : "Hyperlink",
+        choiceD : "Scripture",
+        correct : "JavaScript"
+    },
+
+    {
+        question: "When do JavaScripts execute?",
+        choiceA : "Never",
+        choiceB : "At the end",
+        choiceC : "Immediately as the page loads",
+        choiceD : "After a certain time has passed",
+        correct : "Immediately as the page loads"
+    },
+
+    {
+        question: "Is JavaScript case sensitive?",
+        choiceA : "No",
+        choiceB : "Yes",
+        choiceC : "Maybe",
+        choiceD : "Sometimes",
+        correct : "Yes"    
+    },
+]
+
+// renders question
+var lastQuestionIndex = quizQuestions.length - 1;
+var runningQuestionIndex = 0
+
+function renderQuestion() {
+    var q = quizQuestions[runningQuestionIndex];
+    questionsTitle.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+
+}
 
 
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
 
-    if (storedScores !== null) {
-        allScores = storedScores;
-    }
+    
     startScreen.setAttribute ("style", "display: none");
     startButton.setAttribute ("style", "display: none");
+    renderQuestion();
+    choices.setAttribute("style", "display: block" )
 
+    
+    
+    
     gameTimer()
+    
 }
 
+// Quiz Timer
 function gameTimer() {
     var secondsLeft = 10;
 
@@ -48,3 +97,4 @@ function gameTimer() {
         }
     }, 1000);
 }
+
